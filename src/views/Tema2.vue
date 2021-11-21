00eefffa72e841dcc1f7f92d72759239234570aa
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2021-11-20 22:54:18 -0500

    Tema 5

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -8,6 +8,94 @@
         span 2
       h1 Guión técnico
 
+    .cajon.color-acento-contenido.p-4.mb-4
+      .row.justify-content-center
+        .col-lg-3.col-7.d-flex.align-items-center
+          figure.mb-4
+            img(src='@/assets/curso/tema2/img01.svg') 
+        .col-lg-7.d-flex.align-items-center
+          p.mb-4 Al igual que el storyboard, no hay un formato único para desarrollarlo, más bien responde a las necesidades del producto audiovisual y al equipo de producción. En este documento, a partir del guion literario, se planean cada uno de los planos que compondrán las secuencias, describiendo el tipo de plano, ángulo, movimientos de cámara, sonidos, efectos especiales, diálogos y duración.
+      
+
+    .tabla-a.color-acento-botones.mb-5 
+      table
+        caption Nota: ejemplo de plano de guion técnico
+        thead
+          tr
+            th Secuencia
+            th Plano
+            th(colspan=4) Descripción
+            th Ángulo
+            th Lente
+            th(colspan=2) Diálogo
+            th(colspan=2) Sonido
+            th Duración
+            th Duración total
+        tbody
+          tr
+            td.text-center 1
+            td.text-center 1
+            td(colspan=4) Plano entero (FS). Leve Dolly in hacia el personaje. El personaje mira su reloj y se ve un poco preocupado.
+            td.text-center Normal
+            td.text-center 35 mm
+            td(colspan=2) Personaje 1 - ¿Qué habrá asado? Humm...
+            td(colspan=2) Música de tensión. Sonido de las manecillas del reloj.
+            td.text-center 5"
+            td.text-center 5"
+
+    .row.justify-content-center
+      .col-lg-8
+        p.mb-5.text-center <b>El guion técnico puede ser complementado con otra columna denominada plano de planta, donde se describe de forma gráfica la ubicaciónde los personajes y la cámara, como se muestra a continuación:</b>
+    
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        figure
+          img(src='@/assets/curso/tema2/img02.svg') 
+
+    div.fondo(:style="{'background-image': `url(${require('@/assets/curso/tema2/img03.svg')})`}")
+      div.t2-border.mb-5.py-5.px-4
+        h3 Regla de los 180°
+        p.mb-4 En la imagen anterior, adicional a un plano de planta, se puede visualizar una de las reglas más importantes al momento de componer planos cinematográficos: <b>la regla de los 180°</b>.
+        p.mb-4 Esta regla dicta que existe una línea imaginaria denominada “eje de acción”, la cual se establece por la dirección de las miradas entre los personajes, o también por la dirección del movimiento de un personaje. Una vez dispuesta la cámara a un costado de la acción (ver imagen 2, cámara B), se tiene una referencia espacial de los personajes la cual se debe mantener, con el fin de no confundir al espectador o llegar a romper la continuidad espacial de la escena. Se ve que en la imagen 2, cámara A, B y C, los personajes siempre aparecen al costado izquierdo y derecho del encuadre respectivamente; no obstante, para la cámara D, se observa que la posición de los personajes se intercambió, y de ser estas 4 cámaras, planos de una misma escena, se caería en lo que se denomina “ruptura de eje”.
+        p.mb-0 Esta regla, como todas en el lenguaje audiovisual, se puede romper, pero de manera consciente sabiendo que eso generará caos, confusión o desequilibrio. Para ampliar un poco este tema puede acceder al material complementario de estudio denominado AprenderCine.com (2017), <b>Salto de eje y eje de acción en el cine</b>.
+
+      h3 Consideraciones previas
+      div.mb-5.px-4
+        p.mb-5 Para el desarrollo del guion técnico se deben tener presentes las unidades que permiten separar una acción de otra:
+        .row.mb-4
+          .col-lg-1.p-0.d-flex.justify-content-end
+            figure.num
+              img(src='@/assets/curso/num1.svg') 
+          .col-lg-11.pt-2
+            p.mb-1 <b>Secuencia</b>
+            p.mb-0 Es una unidad narrativa e independiente que posee significado. Tiene su propio inicio, desarrollo y conclusión de una situación dramática. La secuencia puede desarrollarse en un escenario o en varios, así como también puede estar compuesto por una o varias escenas. Puede desarrollarse de manera lineal o narrarse por fragmentos y mezclarse con otras secuencias (NOVASUR).
+            
+        .row.mb-4
+          .col-lg-1.p-0.d-flex.justify-content-end
+            figure.num
+              img(src='@/assets/curso/num2.svg') 
+          .col-lg-11.pt-2
+            p.mb-1 <b>Escena</b>
+            p.mb-0 Unidad de acción que se delimita por el espacio y tiempo. Si la acción cambia de escenario o hay una alteración en el tiempo, es necesario un cambio de escena. Una escena puede estar compuesta por uno o varios planos.
+            
+        .row.mb-4
+          .col-lg-1.p-0.d-flex.justify-content-end
+            figure.num
+              img(src='@/assets/curso/num3.svg') 
+          .col-lg-11.pt-2
+            p.mb-1 <b>Toma</b>
+            p.mb-4 Lapso de tiempo desde que el director dice “acción” y se comienza a grabar, hasta el momento que dice “corten” y se detiene. En una grabación existen muchas tomas hasta que un plano salga como el director lo desea. Más adelante, la toma correcta es seleccionada y combinada por edición (montaje) (NOVASUR).
+            p.mb-0 Usualmente en animación no hay tomas, dado que los planos que se animan se han planeado con rigor y no hay oportunidad, por tiempo y presupuesto, de hacer varias tomas para una misma acción narrativa.
+
+        .row.mb-4
+          .col-lg-1.p-0.d-flex.justify-content-end
+            figure.num
+              img(src='@/assets/curso/num4.svg') 
+          .col-lg-11.pt-2
+            p.mb-1 <b>Plano</b>
+            p.mb-0 Unidad básica del lenguaje audiovisual con significado, donde se define la dimensión de los objetos dentro del encuadre.
+
+
     Separador 
     
     
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -8,6 +8,296 @@
         span 5
       h1 Elevator pitch
 
+    .row.justify-content-center.mb-5.t1-fondo1.t5-border.pb-5
+      .col-10.pt-5.px-4
+        figure.mb-5
+          img(src='@/assets/curso/tema5/img01.png') 
+        p.mb-0 Se le da el nombre de presentación de pitch a una discurso corto y conciso (de 30 segundos a máximo 2 minutos), donde se describe una idea, producto o compañía, con el objetivo de buscar financiación de un posible inversor para desarrollar un proyecto. Es la mejor herramienta para responder a la pregunta ¿qué hace su empresa? o en nuestro caso ¿qué proyecto audiovisual va a realizar? ¿En qué se enfocaría si solo tuviera 30 segundos para mostrar su idea?.
+
+    h3.text-center Consejos para la presentación
+    
+    .row.justify-content-center
+      .col-10
+        PasosA.color-primario.mb-5(tipo="n")
+          .row
+            .col-md-10.mb-4.mb-md-0
+              h4 Sea breve
+              p La presentación de pitch no es un espacio para presentar todos los detalles del proyecto, solo lo esencial. No por esto caiga en el error de hablar demasiado rápido.
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img02.svg')    
+
+          .row
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img03.svg')  
+            .col-md-10.mb-4.mb-md-0
+              h4 Sea persuasivo
+              p No es una presentación de ventas, pero sí pretende convencer o generar expectativa y emoción. Busque tener una introducción explosiva y un nombre atractivo.
+                       
+          .row
+            .col-md-10.mb-4.mb-md-0
+              h4 Tenga una presentación
+              p Realice diapositivas con un diseño único y coherente con el proyecto.
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img04.svg')    
+
+          .row
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img05.svg')  
+            .col-md-10.mb-4.mb-md-0
+              h4 Conozca su audiencia
+              p Realice un estudio de la persona o compañía a la que va a exponer su proyecto e identifique su experiencia o los proyectos en los que suele interesarse.
+                       
+          .row
+            .col-md-10.mb-4.mb-md-0
+              h4 Comparta sus habilidades
+              p Se debe explicar quién es usted o su equipo, cuál es la experiencia y habilidades que poseen.
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img06.svg')    
+
+          .row
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img07.svg')  
+            .col-md-10.mb-4.mb-md-0
+              h4 Practique varias veces
+              p Es la mejor manera para lograr sentirse cómodo y exponerlo de forma natural. Puede presentarlo a un amigo o grabarse, con el fin de recibir opiniones.
+                       
+          .row
+            .col-md-10.mb-4.mb-md-0
+              h4 Sea flexible
+              p Esté abierto a responder posibles preguntas que le puedan hacer en medio de la presentación.
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img08.svg')    
+
+          .row
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img09.svg')  
+            .col-md-10.mb-4.mb-md-0
+              h4 Evite divagar
+              p La clave para no caer en este error es la práctica hasta llegar a interiorizar toda la presentación.
+                       
+          .row
+            .col-md-10.mb-4.mb-md-0
+              h4 Module su tono de voz              
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img10.svg')    
+
+          .row
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img11.svg')  
+            .col-md-10.mb-4.mb-md-0
+              h4 Responda preguntas
+              p Prepárese para responder preguntas específicas con respuestas concisas.
+                       
+          .row
+            .col-md-10.mb-4.mb-md-0
+              h4 No mienta
+              p Si no conoce la respuesta, es mejor no dar información equivocada. Anótelas para llegar a cubrirlas en una próxima presentación.
+            .col-md-2
+              figure
+                img(src='@/assets/curso/tema5/img12.svg')    
+
+    h3.text-center Consejos para la presentación
+        
+    div.px-5.mb-5
+      SlyderA.pb-4
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num1.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Portada
+                  
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Imagen y logo atractivo, que reúna la esencia del proyecto.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Fecha de la presentación para que sea recordada.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Nombres de los integrantes del equipo o el nombre del estudio desarrollador.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Información de contacto: email y/o número telefónico.    
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num2.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Defina el problema
+                  
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Presente de forma directa y concisa el problema que motivó la creación de su proyecto.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | De datos certeros de fuentes de información confiables.
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num3.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Solución
+                  
+                  p.mb-0 ¿Cuál es la solución que usted o su equipo idearon para el problema existente?
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num4.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Target
+                  
+                  p.mb-0 Seguramente al definir el problema y plantear una solución lo ha hecho de acuerdo al público objetivo. En este apartado caracterice a ese segmento de la población al cual quiere dirigirse con su producto.
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num5.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Perfil del estudio o equipo
+                  
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Descripción breve de quién es usted y su experiencia.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Encaso de tenerlas, es bueno mostrar algunas piezas o proyectos en los que ha participado. Siempre y cuando tengan relación con el proyecto a exponer.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | ¿Por qué usted o su equipo son idóneos para ejecutar la solución planteada?.
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num6.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Especificaciones
+                  
+                  p.mb-4 Conceptos básicos de su proyecto:
+                  ul.lista-ul--color
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Tipo de proyecto, técnica, estilo.
+                    li 
+                      i.fas.fa-circle.fa-xs(style="color: #D44F4A")
+                      | Duración, formato y plataforma de distribución.
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num7.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Historia
+                  
+                  p.mb-0 Presentación de la sinopsis de la historia. No necesariamente se tiene que contar el final, es posible presentar los puntos interesantes para generar expectativa. Ser sugerente.
+
+        .row.justify-content-center.mb-0
+          .col-10          
+            .row.justify-content-center
+              .col-lg-4.col-6
+                figure.mb-4
+                  img(src='@/assets/curso/tema5/img13.svg') 
+              .col-lg-7.d-flex.align-items-center
+                div
+                  .row.justify-content-center.pb-2.mb-4.num-bor
+                    .col-2.pe-0
+                      figure.num
+                        img(src='@/assets/curso/num8.svg') 
+                    .col-10.ps-0.pt-2
+                      h3.mb-0 Características diferenciables
+                  
+                  p.mb-0 Presente de 3 a 5 características que hacen de su proyecto algo destacable, innovador o diferente.
+    
+    .row.justify-content-center.mb-0
+      .col-10
+        .t5.cajon.color-primario.p-4
+          p.mb-5 Para ampliar más acerca de un Elevator Pitch puede consultar desde el material complementario o desde la web algunos ejemplos que, aunque describen el discurso de un pitch con fines de financiación, aportan información valiosa y útil al momento de mostrar ideas de proyecto:
+          .row.justify-content-center.mb-0
+            .col-10
+              a.t5.anexo.mb-4(href="https://en.wikipedia.org/wiki/Main_Page" target="_blank")
+                .anexo__icono
+                  img(src="@/assets/curso/link.svg")
+                .anexo__texto
+                  p <b>Ejemplo.</b> Negocios y Empresa (2018, Cómo hacer un Elevator Pitch que enamore al Inversor.
+
+              a.t5.anexo.mb-4(href="https://en.wikipedia.org/wiki/Main_Page" target="_blank")
+                .anexo__icono
+                  img(src="@/assets/curso/link.svg")
+                .anexo__texto
+                  p <b>Ejemplo.</b> LurisdocTV, (2013), Elevator pitch. Tienes 20 segundos - eduCaixa
+
     Separador 
    
 </template>
